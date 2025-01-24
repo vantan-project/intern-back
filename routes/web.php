36cca8028fa6fa0e645ffa21f5e63533b8172946
shimozono-2006 <shimozono.t.24kdgn@gmail.com>
2025-01-24 17:54:45 +0900

    Merge pull request #2 from vantan-project/feature/shimo
    コミット

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\FunaTopController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShimoTopController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("shimo")->group(function(){
+    Route::get('/top',[ShimoTopController::class,'index'])->name("shimo.top.index");
 });
 
 Route::prefix("funa")->group(function () {
     Route::get('/top', [FunaTopController::class, 'index'])->name('funa.top.index');
 });
+
