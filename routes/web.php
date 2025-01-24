38db4e74fef20045b011138860ad32b615b8c785
funaryou <funahashi.ryouma@gmail.com>
2025-01-24 17:56:49 +0900

    ルーティングのコミット

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\FunaTopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("funa")->group(function () {
+    Route::get('/top', [FunaTopController::class, 'index'])->name('funa.top.index');
 });
