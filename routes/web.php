f1c7db88109d0f52ffc9c34c1112badee582473a
ruka42 <yamasakiruka0808@gmail.com>
2025-01-24 17:56:24 +0900

    Feature/ruka (#1)
    * コミットです
    
    * 選び忘れました
    
    ---------
    
    Co-authored-by: ruka <vanilla.0704@icloud.com>
    Co-authored-by: K@teO4I8 <142198827+Kate0418@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RukaController;
 use App\Http\Controllers\FunaTopController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShimoTopController;
@@ -8,7 +9,10 @@ Route::prefix("shimo")->group(function(){
     Route::get('/top',[ShimoTopController::class,'index'])->name("shimo.top.index");
 });
 
+Route::prefix("ruka")->group(function(){
+    Route::get("/top", [RukaController::class, "index"])->name("ruka.top.index");
+});
+
 Route::prefix("funa")->group(function () {
     Route::get('/top', [FunaTopController::class, 'index'])->name('funa.top.index');
 });
-
