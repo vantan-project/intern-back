bd729204037bdcf8654e130279a75d09c3ec69db
ruka <vanilla.0704@icloud.com>
2025-01-24 17:43:02 +0900

    コミットです

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\RukaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("ruka")->group(function(){
+    Route::get("/top", [RukaController::class, "index"])->name("ruka.top.index");
 });
