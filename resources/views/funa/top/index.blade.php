94199f02b3722638d48d00a43a86ec66cacabde3
funaryou <funahashi.ryouma@gmail.com>
2025-01-24 17:45:20 +0900

    表示のコミット

---
+++ resources/views/funa/top/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    funa
+
+    <form action="">
+        <label for="fileUpload">ファイルを選択:</label>
+        <input type="file" id="fileUpload" name="fileUpload" required>←input type="file
+        <button type="submit">アップロード</button>←ボタン
+    </form>
+</body>
+</html>
