8310c26bfb12d8a424e05519fab3d080c283f7a0
ruka42 <yamasakiruka0808@gmail.com>
2025-02-07 11:20:34 +0900

    Feature/ruka/meke model (#5)
    * コミットです
    
    * 選び忘れました
    
    * できたよ
    
    * モデルとマイグレーション書き換えれました
    
    * 代わりにコミット
    
    ---------
    
    Co-authored-by: ruka <vanilla.0704@icloud.com>
    Co-authored-by: K@teO4I8 <142198827+Kate0418@users.noreply.github.com>
    Co-authored-by: Kate0418 <nakao.keito0418@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ Route::prefix("shimo")->group(function(){
     Route::get('/top',[ShimoTopController::class,'index'])->name("shimo.top.index");
 });
 
-Route::prefix("ruka")->group(function(){
-    Route::get("/top", [RukaController::class, "index"])->name("ruka.top.index");
-});
-
 Route::prefix("funa")->group(function () {
     Route::get('/top', [FunaTopController::class, 'index'])->name('funa.top.index');
+});
+
+Route::prefix("ruka")->group(function(){
+    Route::get("/top", [RukaController::class, "index"])->name("ruka.top.index");
 });
